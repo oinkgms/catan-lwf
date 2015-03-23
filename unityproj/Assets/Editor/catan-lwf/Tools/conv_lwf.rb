87d753be27505f352a65c1b6f7fa9a19947f53d6
fum1h1ro <fumihiro@gmail.com>
2015-03-23 15:16:53 +0900

    implove rakefile

--- unityproj/Assets/Editor/catan-lwf/Tools/conv_lwf.rb
+++ unityproj/Assets/Editor/catan-lwf/Tools/conv_lwf.rb
@@ -18,6 +18,10 @@ if SWF_DIR.empty? or LWF_DIR.empty?
   puts "ERROR: set 'SWF_DIR' and 'LWF_DIR'"
   exit 1
 end
+unless HAS_TEXTURE_PACKER
+  puts "ERROR: you don't have a TexturePacker"
+  exit 1
+end
 
 SWF_FILES_ABS = FileList["#{SWF_DIR}/**/*.swf"]
 SWF_FILES_REL = SWF_FILES_ABS.map { |swf| swf.gsub(/#{SWF_DIR}(\/)?/, '') }
@@ -28,45 +32,46 @@ SWF_FILES = SWF_FILES_ABS.zip(SWF_FILES_REL)
 #p File.exist?(SWF2LWF_RB)
 #p SWF_DIR
 #p LWF_DIR
-p SWF_FILES
+#p SWF_FILES
 
-task :hogehoge do
-  p SELF_PATH
-  p SWF2LWF_RB
-end
 
-namespace :lwf do
-  LWFDATA_DIRS = FileList.new
-  BMP_FILES = FileList.new
-  LWF_FILES = FileList.new
-  ROMLWF_FILES = FileList.new
-  ROMBMP_FILES = FileList.new
-  SWF_FILES.each do |pairs|
-    swf = pairs[0]
-    rel = pairs[1]
-    fla = swf.pathmap('%X.fla')
-    lwfdir = swf.pathmap('%X.lwfdata')
-    bmpdir = swf.pathmap('%X.bitmap')
-    bmp = swf.pathmap("#{bmpdir}/%n.bitmap.png")
-    json = swf.pathmap("#{bmpdir}/%n.bitmap.json")
-    lwf = swf.pathmap("#{lwfdir}/%n.lwf")
-    romlwf = rel.pathmap("#{LWF_DIR}/%X.bytes")
-    rombmp = rel.pathmap("#{LWF_DIR}/%X.bitmap.png")
-    romdir = File.dirname(romlwf)
-    LWFDATA_DIRS.include(lwfdir)
-    BMP_FILES.include(bmp)
-    LWF_FILES.include(lwf)
-    ROMLWF_FILES.include(romlwf)
-    ROMBMP_FILES.include(rombmp)
-    CLEAN.include(lwfdir, bmpdir)
-    directory lwfdir
-    directory bmpdir
+LWFDATA_DIRS = FileList.new
+BMP_FILES = FileList.new
+LWF_FILES = FileList.new
+ROMLWF_FILES = FileList.new
+ROMBMP_FILES = FileList.new
+SWF_FILES.each do |pairs|
+  swf = pairs[0]
+  rel = pairs[1]
+  fla = swf.pathmap('%X.fla')
+  lwfdir = swf.pathmap('%X.lwfdata')
+  bmpdir = swf.pathmap('%X.bitmap')
+  bmp = swf.pathmap("#{bmpdir}/%n.bitmap.png")
+  json = swf.pathmap("#{bmpdir}/%n.bitmap.json")
+  lwf = swf.pathmap("#{lwfdir}/%n.lwf")
+  romlwf = rel.pathmap("#{LWF_DIR}/%X.bytes")
+  rombmp = rel.pathmap("#{LWF_DIR}/%X.bitmap.png")
+  romdir = File.dirname(romlwf)
+  LWFDATA_DIRS.include(lwfdir)
+  BMP_FILES.include(bmp)
+  LWF_FILES.include(lwf)
+  ROMLWF_FILES.include(romlwf)
+  ROMBMP_FILES.include(rombmp)
+  CLEAN.include(lwfdir, bmpdir)
+  directory lwfdir
+  directory bmpdir
+  namespace :lwf do
     file bmp => [swf, lwfdir, bmpdir] do |t| # extract texture and generate atlus
       sh "ruby #{SWF2LWF_RB} -p #{t.prerequisites[0]}"
       texs = FileList["#{t.prerequisites[1]}/*.png"]
       sh "#{TEXTURE_PACKER} #{TEXTURE_PACKER_OPTS} --format json --data #{json} --sheet #{bmp} #{texs}"
       rm "#{lwf}"
     end
+  end
+  lwftask = File.dirname(swf) + '/' + File.basename(swf, ".*")
+  desc "Convert #{swf}"
+  task lwftask => [romlwf, rombmp]
+  namespace :lwf do
     file lwf => [swf, bmp] do |t| #
       if File.exist?(fla)
         sh "ruby #{SWF2LWF_RB} -f #{fla} #{swf} #{json}"
@@ -82,14 +87,15 @@ namespace :lwf do
       sh "cp #{t.prerequisites[1]} #{t.name}"
     end
   end
+end
 
-  #p LWFDATA_DIRS
-  #p BMP_FILES
-  #p LWF_FILES
-  #p ROMBMP_FILES
-  #p ROMLWF_FILES
+#p LWFDATA_DIRS
+#p BMP_FILES
+#p LWF_FILES
+#p ROMBMP_FILES
+#p ROMLWF_FILES
 
-
+namespace :lwf do
   desc "extract bitmap from swf"
   task :bmp => BMP_FILES
   desc "convert swf to lwf"
@@ -99,62 +105,4 @@ namespace :lwf do
   desc "install converted lwf and png"
   task :install => ROMLWF_FILES + ROMBMP_FILES
 end
-
-__END__
-BITMAP_ATLUSES = FileList.new
-BITMAP_DIRS.each do |d|
-  #p "hoge #{File.basename(d)}"
-  if Dir.exist?(d)
-    atlus = "#{TMPDIR}/#{File.basename(d)}.png"
-    json = "#{TMPDIR}/#{File.basename(d)}.json"
-    texs = FileList["#{d}/*.png"]
-    BITMAP_ATLUSES.include(atlus)
-    file atlus => texs do |t|
-      sh "#{TEXTURE_PACKER} --scale 1.0 --format json --data #{json} --sheet #{atlus} #{texs}"
-    end
-    CLEAN.include(atlus)
-  end
-end
-
-LWF_FILES = FileList.new
-ROM_FILES = FileList.new
-SWF_FILES.each do |swf|
-  #p swf
-  lwf_dir = swf.pathmap("%X.lwfdata")
-  lwf = swf.pathmap("#{lwf_dir}/%X.lwf")
-  lwf_rom = swf.pathmap("#{ROMDIR}/%X.bytes")
-  #p lwf
-  src = FileList.new
-  src << swf
-  atlus_tmp = swf.pathmap("#{TMPDIR}/%X.bitmap.png")
-  atlus_rom = swf.pathmap("#{ROMDIR}/%X.bitmap.png")
-  json = swf.pathmap("#{TMPDIR}/%X.bitmap.json")
-  src << atlus_tmp if BITMAP_ATLUSES.include?(atlus_tmp)
-  #p "LWF #{lwf}"
-  #p "SRC #{src}"
-  LWF_FILES.include(lwf)
-  file lwf => src do |t|
-    sh "ruby #{SWF2LWF_RB} #{swf} #{json}"
-  end
-  unless ROMDIR.empty?
-    p ROMDIR
-    #LWF_FILES.each do
-    if BITMAP_ATLUSES.include?(atlus_tmp)
-      file atlus_rom => atlus_tmp do |t|
-        sh "cp #{t.prerequisites[0]} #{t.name}"
-        p t
-      end
-      ROM_FILES << atlus_rom
-    end
-    file lwf_rom => lwf do |t|
-      sh "cp #{t.prerequisites[0]} #{t.name}"
-      p t.name
-      p t.prerequisites
-    end
-    ROM_FILES << lwf_rom
-  end
-  CLEAN.include(lwf_dir)
-end
-
-
 
